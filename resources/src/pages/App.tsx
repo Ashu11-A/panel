d521d9c932f225c5da6b5e6c7bd5d911a789be90
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2024-08-19 16:35:50 -0300

    🪄 Add ShadcnUI

--- resources/src/pages/App.tsx
+++ resources/src/pages/App.tsx
@@ -1,8 +1,10 @@
 const valor = import.meta.env.VITE_DEVELOPMENT_BACKEND_URL
+
 export default function App() {
   return (
     <div className='min-h-[90vh] text-center content-center'>
       <p>Hello world {valor}</p>
+      <Button></Button>
     </div>
   )
 }
--- resources/vite.config.ts
+++ resources/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import env from './env'
 import path from 'path'
+
 const backend_api = env.read('VITE_DEVELOPMENT_BACKEND_URL')
 // https://vitejs.dev/config/
 export default defineConfig({
